990d71338c8c345fa30e368cfac16d7e695209bf
Zachary Lee <zacharylhlee@gmail.com>
2018-11-09 20:06:05 +0000

    Prevent user sign up with existing e-mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,15 @@ class UsersController < ApplicationController
   end
   def create
     user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
+    if User.exists?( email: user.email )
       redirect_to '/signup'
+    else
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
     end
   end
 
