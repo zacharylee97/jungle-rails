f1cbd2290a277688bb8fa8580643293fb44d6812
Zachary Lee <zacharylhlee@gmail.com>
2018-11-09 21:01:09 +0000

    Adds rating field

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,11 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @review = Review.new
     @reviews = Review.where(product_id: params[:id]).order(created_at: :desc)
+    @rating = average_rating(@reviews)
   end
 
+  private
+  def average_rating(reviews)
+    reviews.ratings.average(:ratings)
+  end
 end
