d281778a36e4a16d76293e823eccf3925ce11806
Zachary Lee <zacharylhlee@gmail.com>
2018-11-12 16:20:45 +0000

    Adds validations for email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,18 @@ RSpec.describe User, type: :model do
                       password: "my_password", password_confirmation: "my_password")
       expect(@user.save).to eq false
     end
+    it "is not valid without email" do
+      @user = User.new(first_name: "first_name", last_name: "last_name", email: nil,
+                      password: "my_password", password_confirmation: "my_password")
+      expect(@user.save).to eq false
+    end
+    it "should not save a user that has the same email" do
+      @user1 = User.new(first_name: "first_name", last_name: "last_name", email: "TEST@TEST.com",
+                        password: "my_password", password_confirmation: "my_password")
+      @user1.save
+      @user2 = User.new(first_name: "first_name", last_name: "last_name", email: "test@test.COM",
+                        password: "my_password", password_confirmation: "my_password")
+      expect(@user2.save).to eq false
+    end
   end
 end
