c927278c0a528732178abcde1df6b5b328a89261
Zachary Lee <zacharylhlee@gmail.com>
2018-11-12 16:13:24 +0000

    Adds validations for first_name and last_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe User, type: :model do
                       password: "my_password", password_confirmation: "my_password")
       expect(@user.save).to eq true
     end
+    it "is not valid without first_name" do
+      @user = User.new(first_name: nil, last_name: "last_name", email: "my_email@email.com",
+                      password: "my_password", password_confirmation: "my_password")
+      expect(@user.save).to eq false
+    end
+    it "is not valid without last_name" do
+      @user = User.new(first_name: "first_name", last_name: nil, email: "my_email@email.com",
+                      password: "my_password", password_confirmation: "my_password")
+      expect(@user.save).to eq false
+    end
   end
 end
